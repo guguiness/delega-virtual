7055b1dce40ce3f2caee273c8dfbed68a629d92a
guguiness <gus.nogueira@protonmail.com>
2022-12-16 14:41:11 -0300

    Adicionando constraints de tabelas restantes

--- scripts/tabelas.sql
+++ scripts/tabelas.sql
@@ -8,7 +8,12 @@ CREATE TABLE pessoa (
 	CONSTRAINT pk_pessoa PRIMARY KEY (id_pessoa)
 );
 
-SELECT * FROM pessoa;
+
+
+SELECT
+	*
+FROM
+	pessoa;
 
 CREATE TABLE endereco (
     id_end integer NOT NULL,
@@ -17,7 +22,10 @@ CREATE TABLE endereco (
     CONSTRAINT pk_endereco PRIMARY KEY (id_end)
 );
 
-SELECT * FROM endereco;
+SELECT
+	*
+FROM
+	endereco;
 
 CREATE TABLE ocorrencia (
     id_ocorrencia integer,
@@ -29,58 +37,97 @@ CREATE TABLE ocorrencia (
     CONSTRAINT fk_ocorrencia_pk_endereco FOREIGN KEY (id_end) REFERENCES endereco(id_end)
 );
 
-SELECT * FROM ocorrencia;
+SELECT
+	*
+FROM
+	ocorrencia;
 
--- TODO
 CREATE TABLE vitima_ocorrencia (
-    id_pessoa integer, /*chave estrangeira*/
-id_ocorrencia integer /*chave estrangeira*/
+    id_pessoa integer,
+	id_ocorrencia integer,
+    CONSTRAINT fk_vitima_ocorrencia_pk_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    CONSTRAINT fk_vitima_ocorrencia_pk_ocorrencia FOREIGN KEY (id_ocorrencia) REFERENCES ocorrencia(id_ocorrencia)
 );
+
+SELECT
+	*
+FROM
+	vitima_ocorrencia;
 
 CREATE TABLE infrator_ocorrencia (
-    id_pessoa integer NOT NULL,/*chave estrangeira*/
-	id_ocorrencia integer NOT NULL /*chave estrangeira*/
+    id_pessoa integer NOT NULL,
+	id_ocorrencia integer NOT NULL,
+    CONSTRAINT fk_infrator_ocorrencia_pk_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    CONSTRAINT fk_infrator_ocorrencia_pk_ocorrencia FOREIGN KEY (id_ocorrencia) REFERENCES ocorrencia(id_ocorrencia)
 );
+
+SELECT
+	*
+FROM
+	infrator_ocorrencia;
 
 CREATE TABLE arma (
-    id_arma integer NOT NULL UNIQUE, /*chave primária*/
+    id_arma integer NOT NULL UNIQUE,
 	modelo varchar(50) NOT NULL,
-    situacao varchar(20) NOT NULL
+    situacao varchar(20) NOT NULL,
+    CONSTRAINT pk_arma PRIMARY KEY (id_arma)
 );
+
+SELECT
+	*
+FROM
+	arma;
 
 CREATE TABLE arma_ocorrencia (
-    id_ocorrencia integer NOT NULL UNIQUE, /*chave estrangeira*/
-	id_arma integer NOT NULL /*chave estrangeira*/
+	id_arma integer NOT NULL,
+    id_ocorrencia integer NOT NULL,
+    CONSTRAINT fk_arma_ocorrencia_pk_arma FOREIGN KEY (id_arma) REFERENCES arma(id_arma),
+    CONSTRAINT fk_arma_ocorrencia_pk_ocorrencia FOREIGN KEY (id_ocorrencia) REFERENCES ocorrencia(id_ocorrencia)
 );
 
+SELECT
+	*
+FROM
+	arma_ocorrencia;
+
 CREATE TABLE denuncia (
-    id_denuncia integer NOT NULL UNIQUE,/*chave primária*/
+    id_denuncia integer NOT NULL,
 	dt_denuncia date NOT NULL,
-    motivo varchar(255) NOT NULL
+    motivo varchar(255) NOT NULL,
+    CONSTRAINT pk_denuncia PRIMARY KEY (id_denuncia)
 );
 
 CREATE TABLE denuncia_denunciado (
-    id_pessoa integer,/* chave estrangeira */
-	id_denuncia integer /* chave estrangeira */
+	id_denuncia integer,
+    id_pessoa integer,
+	CONSTRAINT fk_denuncia_denunciado_pk_denuncia FOREIGN KEY (id_denuncia) REFERENCES denuncia(id_denuncia),
+	CONSTRAINT fk_denuncia_denunciado_pk_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa)
 );
 
 CREATE TABLE veiculo (
-    id_veiculo integer NOT NULL,/*chave primária*/
+    id_veiculo integer NOT NULL,
 	renavam varchar(25) NOT NULL,
     marca varchar(20),
     ano_fab integer,
-    id_proprietario integer NOT NULL /*chave estrangeira*/
+    id_proprietario integer NOT NULL,
+    CONSTRAINT pk_veiculo PRIMARY KEY (id_veiculo),
+    CONSTRAINT fk_veiculo_pk_pessoa FOREIGN KEY (id_proprietario) REFERENCES pessoa(id_pessoa)
 );
 
 CREATE TABLE apreensao (
-    id_apreensao integer,/*chave primária*/
-	id_veiculo integer,/*chave estrangeira*/
-	id_ocorrencia integer,/*chave estangeira*/
+    id_apreensao integer,
+	id_veiculo integer,
+	id_ocorrencia integer,
 	infracao varchar(20),
-    situacao varchar(20)
+    situacao varchar(20),
+    CONSTRAINT pk_apreensao PRIMARY KEY (id_apreensao),
+    CONSTRAINT fk_apreensao_pk_veiculo FOREIGN KEY (id_veiculo) REFERENCES veiculo(id_veiculo),
+    CONSTRAINT fk_apreensao_pk_ocorrencia FOREIGN KEY (id_ocorrencia) REFERENCES ocorrencia(id_ocorrencia)
 );
 
 CREATE TABLE policial_apreensao (
-    id_pessoa integer,/*chave estrangeira*/
-	id_apreensao integer /*chave estrangeira*/
+    id_pessoa integer,
+	id_apreensao integer,
+	CONSTRAINT fk_policial_apreensao_pk_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+	CONSTRAINT fk_policial_apreensao_pk_apreensao FOREIGN KEY (id_apreensao) REFERENCES apreensao(id_apreensao)
 );
